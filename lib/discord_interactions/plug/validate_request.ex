b12ce3302e17413dce79ea35556c301f7676650d
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2025-04-29 14:58:06 +0200

    fix: wrong assign in request validation

--- lib/discord_interactions/plug/validate_request.ex
+++ lib/discord_interactions/plug/validate_request.ex
@@ -18,7 +18,7 @@ defmodule DiscordInteractions.Plug.ValidateRequest do
     with {:key, public_key} when public_key != nil <-
            {:key, Application.get_env(:discord_interactions, :public_key)},
          {:key, {:ok, public_key}} <- {:key, Base.decode16(public_key, case: :mixed)},
-         {:body, body} when body != nil <- {:body, conn.assigns[:raw_body]},
+         {:body, [body]} when body != nil <- {:body, conn.assigns[:raw_body]},
          {:headers, [signature]} <- {:headers, get_req_header(conn, "x-signature-ed25519")},
          {:headers, [timestamp]} <- {:headers, get_req_header(conn, "x-signature-timestamp")},
          {:headers, {:ok, signature}} <- {:headers, Base.decode16(signature, case: :mixed)},
