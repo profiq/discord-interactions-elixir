17072cf3c1ad849c87e3d9be39d1c328b61ab7f5
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2025-04-29 13:29:42 +0200

    feat: command registration task

---
+++ lib/discord_interactions/command_registration.ex
@@ -0,0 +1,33 @@
+defmodule DiscordInteractions.CommandRegistration do
+  use Task, restart: :transient
+
+  require Logger
+  alias DiscordInteractions.API
+
+  def start_link(handler) do
+    Task.start_link(__MODULE__, :register_commands, [handler])
+  end
+
+  def register_commands(handler) do
+    with commands <- apply(handler, :init, []),
+         {:bot_token, {:ok, bot_token}} <- {:bot_token, Application.fetch_env(:discord_interactions, :bot_token)},
+         {:application_id, {:ok, application_id}} <- {:application_id, Application.fetch_env(:discord_interactions, :application_id)},
+         client <- API.new(token: bot_token, application_id: application_id),
+         {:response, {:ok, _}} <- {:response, API.bulk_overwrite_global_commands(client, commands)}
+    do
+      :ok
+    else
+      {:bot_token, _} ->
+        Logger.error("Discord bot token is not configured")
+        raise "Discord bot token is not configured"
+
+      {:application_id, _} ->
+        Logger.error("Discord application ID is not configured")
+        raise "Discord application id is not configured"
+
+      {:response, {:error, response}} ->
+        Logger.error("Failed to register commands: #{inspect(response)}")
+        raise "Failed to register commands"
+    end
+  end
+end
