7fe06c63d5e9f9b1bbd67f1386cb34553db0e604
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2025-04-29 15:13:42 +0200

    fix: return 500 for config errors

--- lib/discord_interactions/plug/validate_request.ex
+++ lib/discord_interactions/plug/validate_request.ex
@@ -15,10 +15,11 @@ defmodule DiscordInteractions.Plug.ValidateRequest do
 
   @spec call(Plug.Conn.t(), Plug.opts()) :: Plug.Conn.t()
   def call(conn, _opts) do
+    IO.inspect(conn)
     with {:key, public_key} when public_key != nil <-
            {:key, Application.get_env(:discord_interactions, :public_key)},
          {:key, {:ok, public_key}} <- {:key, Base.decode16(public_key, case: :mixed)},
-         {:body, [body]} when body != nil <- {:body, conn.assigns[:raw_body]},
+         {:body, [body]} <- {:body, conn.assigns[:raw_body]},
          {:headers, [signature]} <- {:headers, get_req_header(conn, "x-signature-ed25519")},
          {:headers, [timestamp]} <- {:headers, get_req_header(conn, "x-signature-timestamp")},
          {:headers, {:ok, signature}} <- {:headers, Base.decode16(signature, case: :mixed)},
@@ -28,11 +29,11 @@ defmodule DiscordInteractions.Plug.ValidateRequest do
     else
       {:key, _} ->
         Logger.error("Discord public key is not configured")
-        error(conn, :unauthorized)
+        error(conn, :internal_server_error)
 
       {:body, _} ->
         Logger.error("Could not retrieve cached request body, is the body reader configured?")
-        error(conn, :unauthorized)
+        error(conn, :internal_server_error)
 
       {:headers, _} ->
         Logger.warning("Discord interaction request misses required security headers")
