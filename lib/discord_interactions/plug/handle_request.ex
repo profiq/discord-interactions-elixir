f72fcbf80e98bf9c26f8f177a2a80a6737692120
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2025-05-19 13:33:49 +0200

    feat: handle command handler crashes, add plug tests

--- lib/discord_interactions/plug/handle_request.ex
+++ lib/discord_interactions/plug/handle_request.ex
@@ -6,36 +6,48 @@ defmodule DiscordInteractions.Plug.HandleRequest do
   import DiscordInteractions.Util
   import Plug.Conn
 
+  require Logger
+
+  alias DiscordInteractions.InteractionResponse
+
   def init(opts), do: opts
 
   def call(%{body_params: %{"type" => 1}} = conn, _opts) do
-    resp_body = Jason.encode_to_iodata!(%{type: 1})
-
-    conn
-    |> resp(200, resp_body)
-    |> put_resp_header("content-type", "application/json")
-    |> send_resp()
-    |> halt()
+    send_json(conn, InteractionResponse.pong())
   end
 
   def call(conn, _opts) do
-    case conn.assigns[:discord_command_handler].handle(conn.body_params) do
-      :ok ->
-        # send 202
-        error(conn, :accepted)
+    try do
+      case conn.assigns[:discord_command_handler].handle(conn.body_params) do
+        :ok ->
+          # send 202
+          error(conn, :accepted)
 
-      {:ok, response} ->
-        resp_body =
-          Jason.encode_to_iodata!(response)
+        {:ok, response} ->
+          send_json(conn, response)
 
+        other ->
+          Logger.error("Discord command handler returned unexpected value: #{inspect(other)}")
+          error(conn, :internal_server_error)
+      end
+    rescue
+      exception ->
+        Logger.error("Discord command handler crashed: #{inspect(exception)}\n#{Exception.format_stacktrace(__STACKTRACE__)}")
+        error(conn, :internal_server_error)
+    end
+  end
+
+  defp send_json(conn, response) do
+    case Jason.encode_to_iodata(response) do
+      {:ok, resp_body} ->
         conn
         |> resp(200, resp_body)
         |> put_resp_header("content-type", "application/json")
         |> send_resp()
         |> halt()
 
-      _ ->
-        # send 500
+      {:error, error} ->
+        Logger.error("Failed to encode response: #{inspect(error)}")
         error(conn, :internal_server_error)
     end
   end
---
+++ test/discord_interactions/plug/handle_request_test.exs
@@ -0,0 +1,100 @@
+defmodule DiscordInteractions.Plug.HandleRequestTest do
+  use ExUnit.Case, async: true
+  use Mimic
+
+  import Plug.Conn
+
+  alias DiscordInteractions.Plug.HandleRequest
+
+  setup do
+    %{
+      conn: Plug.Test.conn(:post, "/discord", "")
+    }
+  end
+
+  describe "call/2" do
+    test "handles ping interactions", %{conn: conn} do
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> assign(:discord_command_handler, nil)
+        |> Map.put(:body_params, %{"type" => 1})
+        |> HandleRequest.call(%{})
+
+      assert conn.status == 200
+      assert conn.halted
+      assert conn.resp_body =~ ~s("type":1)
+    end
+
+    test "handles successful command responses", %{conn: conn} do
+      # Define handler for this specific test
+      defmodule SuccessHandler do
+        alias DiscordInteractions.InteractionResponse
+        def handle(_interaction), do: {:ok, InteractionResponse.channel_message_with_source() |> InteractionResponse.content("Success")}
+      end
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> assign(:discord_command_handler, SuccessHandler)
+        |> Map.put(:body_params, %{"type" => "success"})
+        |> HandleRequest.call(%{})
+
+      assert conn.status == 200
+      assert conn.halted
+      assert conn.resp_body =~ ~s("type":4)
+      assert conn.resp_body =~ ~s("content":"Success")
+    end
+
+    test "handles accepted responses", %{conn: conn} do
+      # Define handler for this specific test
+      defmodule AcceptedHandler do
+        def handle(_interaction), do: :ok
+      end
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> assign(:discord_command_handler, AcceptedHandler)
+        |> Map.put(:body_params, %{"type" => "accepted"})
+        |> HandleRequest.call(%{})
+
+      assert conn.status == 202
+      assert conn.halted
+    end
+
+    test "handles error responses", %{conn: conn} do
+      # Define handler for this specific test
+      defmodule ErrorHandler do
+        def handle(_interaction), do: :error
+      end
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> assign(:discord_command_handler, ErrorHandler)
+        |> Map.put(:body_params, %{"type" => "error"})
+        |> HandleRequest.call(%{})
+
+      assert conn.status == 500
+      assert conn.halted
+    end
+
+    test "handles exceptions in command handlers", %{conn: conn} do
+      # Define handler for this specific test
+      defmodule CrashHandler do
+        def handle(_interaction), do: raise "Test exception"
+      end
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> assign(:discord_command_handler, CrashHandler)
+        |> Map.put(:body_params, %{"type" => "crash"})
+        |> HandleRequest.call(%{})
+
+      assert conn.status == 500
+      assert conn.halted
+    end
+  end
+end
